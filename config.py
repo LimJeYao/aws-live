c0212d0b2fd35a906d50a0dfddeb79b24f4609bc
LimJeYao <144700360+LimJeYao@users.noreply.github.com>
2024-04-16 13:54:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c7umo4qa0xog.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "limjeyao-employee"
+customregion = "us-east-1"
+
